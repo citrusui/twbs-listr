7228ffb1c8e85610c9de9b434ea437c18da95e9c
Avery Magnotti <citrusui@users.noreply.github.com>
2016-07-25 12:34:09 -0400

    Bump Bootstrap to 3.3.7

--- index.php
+++ index.php
@@ -136,7 +136,7 @@ return $text;
 <html>
   <head>
     <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   </head>
   <title>Index of 
     <?
